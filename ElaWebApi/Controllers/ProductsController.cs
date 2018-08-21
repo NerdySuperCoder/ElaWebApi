4966a8ad7c0f9e9f14f56093d9b1e91d1889a502
Ela <elamathi@gmail.com>
2018-08-21 23:52:13 +1000

    Checking in some minor action methods

--- ElaWebApi/Controllers/ProductsController.cs
+++ ElaWebApi/Controllers/ProductsController.cs
@@ -10,19 +10,23 @@ namespace ElaWebApi.Controllers
 {
 	public class ProductsController : ApiController
 	{
-		Product[] products = new Product[]
+		List<Product> products = new List<Product>
 		{
 			new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 },
 			new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M },
 			new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M }
 		};
 
-		public IEnumerable<Product> GetAllProducts()
+        [HttpGet]
+        [ActionName("GetAllProducts")]
+		public IEnumerable<Product> GetProducts()
 		{
 			return products;
 		}
 
-		public IHttpActionResult GetProduct(int id)
+        [HttpGet]
+        [ActionName("GetProduct")]
+		public IHttpActionResult GetSingleProduct(int id)
 		{
 			var product = products.FirstOrDefault((p) => p.Id == id);
 			if (product == null)
@@ -31,5 +35,38 @@ namespace ElaWebApi.Controllers
 			}
 			return Ok(product);
 		}
+
+        [HttpDelete]
+        [ActionName("DeleteProduct")]
+        public IHttpActionResult DeleteSingleProduct(int id)
+        {
+            var product = products.FirstOrDefault((p) => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            products.Remove(product);
+
+            return Ok(product);
+        }
+
+        public void SaveProduct(string Name, string Category, int Price)
+        {
+            var maxProdId = products.Max(t => t.Id);
+
+            products.Add(new Product
+            {
+                Id = maxProdId + 1,
+                Name = Name,
+                Category = Category,
+                Price = Price
+            });
+        }
+
+        [NonAction]
+        public void DontCallMeFunction()
+        {
+            //Not implemented
+        }
 	}
 }
